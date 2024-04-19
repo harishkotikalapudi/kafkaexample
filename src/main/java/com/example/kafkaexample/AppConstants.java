ef5aa0eefb693c7c342c7b84d28f1c1b6e232417
Harish <harish.kotikalapudi@gmail.com>
2024-04-19 19:38:43 +0530

    Basic Kafka Example

---
+++ src/main/java/com/example/kafkaexample/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.kafkaexample;
+
+public class AppConstants {
+    public static final String TOPIC_NAME = "kafkaExampleTopic";
+    public static final String GROUP_ID = "group_id";
+}
